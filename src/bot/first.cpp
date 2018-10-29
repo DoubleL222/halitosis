1fe0163ac17410fd0c9dfc1b25690838542327d2
Michael <miev@itu.dk>
2018-10-29 15:36:53 +0100

    Remove old commented code

--- src/bot/first.cpp
+++ src/bot/first.cpp
@@ -71,27 +71,3 @@ std::vector<hlt::Command> FirstBot::run(const hlt::Game& game) {
     }
     return commands;
 }
-
-
-/*
-        std::unique_ptr<hlt::GameMap>& game_map = game.game_map;
-
-        std::vector<hlt::Command> command_queue;
-
-        for (const auto& ship_iterator : me->ships) {
-            shared_ptr<Ship> ship = ship_iterator.second;
-            if (game_map->at(ship)->halite < constants::MAX_HALITE / 10 || ship->is_full()) {
-                Direction random_direction = ALL_CARDINALS[rng() % 4];
-            } else {
-                command_queue.push_back(ship->stay_still());
-            }
-        }
-
-        if (
-            game.turn_number <= 200 &&
-            me->halite >= constants::SHIP_COST &&
-            !game_map->at(me->shipyard)->is_occupied())
-        {
-            command_queue.push_back(me->shipyard->spawn());
-        }
-*/
