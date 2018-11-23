eb5ed4d1e5c393b09b1ffec0c8b1d72d97e65ad8
Jonas Tingmose <ultroman@gmail.com>
2018-11-23 08:38:51 +0100

    Not much

--- src_py/mcts/mcts.py
+++ src_py/mcts/mcts.py
@@ -4,39 +4,58 @@ import hlt
 # This library contains direction metadata to better interface with the game.
 from hlt.positionals import Direction
 
+import math
+import time
 
-def defaultPolicyFn(state):
-    return state.getReward()
+ship_commands = {'n', 's', 'e', 'w', 'o', 'c'}
 
 class TreeNode:
+
     def __init__(self, state, parent):
         self.state = state
-        self.isTerminal = state.isTerminal()
-        self.isFullyExpanded = self.isTerminal
+        self.isGameOver = state.isGameOver()
+        self.isFullyExpanded = self.isGameOver
         self.parent = parent
         self.numVisits = 0
         self.totalReward = 0
         self.children = {}
+        self.untried_actions = {'g'}
+        # Fill untried_actions
+        player = state.players[state.my_id]
+        ships = player.get_ships()
+
+        for ship in ships:
+            for command in ship_commands:
+                self.untried_actions.add(str(ship.id) + "||" + str(command))
+
+    def get_untried_action(self):
+        if self.untried_actions.__len__() == 1:
+            self.isFullyExpanded = True
+        return self.untried_actions.pop()
 
 class Mcts:
-    def __init__(self, timeLimit=None, iterationLimit=None, explorationConstant=1 / math.sqrt(2),
-                 defaultPolicy=defaultPolicyFn):
-        if timeLimit is not None:
-            if iterationLimit is not None:
+    def default_policy_fn(self, state):
+        return state.getReward()
+
+    def __init__(self, time_limit=None, iteration_limit=None, exploration_constant=1 / math.sqrt(2),
+                 default_policy=default_policy_fn):
+        if time_limit is not None:
+            if iteration_limit is not None:
                 raise ValueError("Cannot have both a time limit and an iteration limit")
             # time taken for each MCTS search in milliseconds
-            self.timeLimit = timeLimit
+            self.timeLimit = time_limit
             self.limitType = 'time'
         else:
-            if iterationLimit is None:
+            if iteration_limit is None:
                 raise ValueError("Must have either a time limit or an iteration limit")
             # number of iterations of the search
-            if iterationLimit < 1:
+            if iteration_limit < 1:
                 raise ValueError("Iteration limit must be greater than one")
-            self.searchLimit = iterationLimit
+            self.searchLimit = iteration_limit
             self.limitType = 'iterations'
-            self.explorationConstant = explorationConstant
-            self.defaultPolicy = defaultPolicy
+            self.explorationConstant = exploration_constant
+            self.defaultPolicy = default_policy
+            self.rootNode = None
 
     # function UCTSEARCH(s0)
     #     create root node v0 with state s0
@@ -50,15 +69,15 @@ class Mcts:
         # Create root node with initialState
         self.rootNode = TreeNode(initialState, None)
         if self.limitType == 'time':
-            timeLimit = time.time() + self.timeLimit / 1000
-            while time.time() < timeLimit:
+            time_limit = time.time() + self.timeLimit / 1000
+            while time.time() < time_limit:
                 self.do_uct_search()
         else:
             for i in range(self.searchLimit):
                 self.do_uct_search()
 
-        bestChild = self.getBestChild(self.root, 0)
-        return self.getAction(self.root, bestChild)
+        best_child = self.getBestChild(self.root, 0)
+        return self.getAction(self.root, best_child)
 
     def do_uct_search(self):
         node = self.selectNode(self.root)
@@ -82,7 +101,23 @@ class Mcts:
 
 
     def expand(self, node):
-        node.state.get_expansion_states
+        #node.state.get_expansion_states
+        action = node.get_untried_action()
+        node.children.add(self.generate_new_node(node, action))
+
+
+
+    def generate_new_node(self, node, action):
+        player = node.state.players[node.state.my_id]
+        ships = player.get_ships()
+
+        for ship in ships:
+            for command in ship_commands:
+                self.untried_actions.add(str(ship.id) + "||" + str(command))
+        if action == 'g':
+            # Our action is to generate a ship
+            node.state.
+        else:
 
     # function BESTCHILD(v, c)
     # return (big calculation)
@@ -98,10 +133,3 @@ class Mcts:
     #         N(v) ← N(v) + 1
     #         Q(v) ← Q(v) + ∆(v, p)
     #         v ← parent of v
-
-    def get_possible_actions(self, node, state):
-        player = state.players[state.my_id]
-        ships = player.get_ships()
-        for ship in ships:
-            for dir in Direction.
-            node[str(ship.id) + str()]
