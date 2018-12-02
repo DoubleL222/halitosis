e48c44b9fdab98df9126a272693e9807136cf48e
Luka Locniskar <luka.locniskar@gmail.com>
2018-12-02 23:28:06 +0100

    direction fixes

--- src_py/run_game_sim_test.bat
+++ src_py/run_game_sim_test.bat
@@ -1,2 +1,2 @@
-halite.exe --replay-directory replays/ -vvv --width 6 --height 6 --no-timeout "python MyBotSimTest.py" "python MyBotSimTest.py"
+halite.exe --replay-directory replays/ -vvv --width 6 --height 12 --no-timeout "python MyBotSimTest.py" "python MyBotSimTest.py"
 pause
